87f6fb476925899082dc22f4c4a0e73833b9df43
Huguinho <up201404587@fe.up.pt>
2017-06-03 23:49:04 +0100

    C code generator done

--- src/TacToC.java
+++ src/TacToC.java
@@ -2,69 +2,80 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class TacToC
 {
-	public static String toCString(String fileName) throws FileNotFoundException, IOException
+	public static void toCString(String fileName, PrintWriter out) throws FileNotFoundException, IOException
 	{
 		ArrayList<String> declaredVariables = new ArrayList<String>();
-		String codeC = "int main()\n{\n";
+		out.println("int main()\n{\n");
 
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName)))
 		{
 			String line;
 			String newCcodeLine;
-			
+
 			while ((line = br.readLine()) != null)
 			{
 				newCcodeLine = "";
 				String[] parts = line.split(" ");
-				switch(parts.length)
+				switch (parts.length)
 				{
-					case 4:
-					{
-						if (!declaredVariables.contains(parts[0]))
-						{
-							newCcodeLine += "double" + " " + parts[0] + " = " + parts[1] + " " + parts[2] + " " + parts[3] + ";\n";
-						}
-						break;
-					}
-					case 2:
-					{
-						if (!declaredVariables.contains(parts[0]))
-						{
-							newCcodeLine += "double" + " " + parts[0] + " = " + parts[1] + " " + parts[2] + " " + parts[3] + ";\n";
-						}
-						break;
-					}
-					case 6:
-					{
-						
-						break;
-					}
-					case 1:
-					{
-						
-						break;
-					}
-					default:
-					{
-						System.out.println("Que linha e esta?!?");
-						break;
-					}
+				case 1:
+				{
+					newCcodeLine += parts[0] + " return;";
+					break;
 				}
-				codeC += newCcodeLine;
+				case 5:
+				{
+					if (!declaredVariables.contains(parts[0]))
+					{
+						newCcodeLine += "double ";
+						declaredVariables.add(parts[0]);
+					}
+					newCcodeLine += parts[0] + " = " + parts[2] + " " + parts[3] + " " + parts[4] + ";\n";
+					break;
+				}
+				case 3:
+				{
+					if (!declaredVariables.contains(parts[0]))
+					{
+						newCcodeLine += "double ";
+						declaredVariables.add(parts[0]);
+					}
+					newCcodeLine += parts[0] + " = " + parts[2] + ";\n";
+					break;
+				}
+				case 7:
+				{
+					newCcodeLine += parts[0] + " " + parts[1] + " (" + parts[2] + " " + parts[3] + " " + parts[4] + ") "
+							+ parts[5] + " " + parts[6] + ";\n";
+					break;
+				}
+				case 2:
+				{
+					newCcodeLine += parts[0] + " " + parts[1] + ";\n";
+					break;
+				}
+				default:
+				{
+					System.out.println("Que linha e esta?!?");
+					break;
+				}
+				}
+				out.println(newCcodeLine);
 			}
 		}
-
-		codeC += "\n}";
-		System.out.println(codeC);
-		return codeC;
+		out.println("\n}");
 	}
+	
 
 	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
-		toCString("input.txt");
+		PrintWriter out = new PrintWriter("out.txt");
+		toCString("input.txt", out);
+		out.close();
 	}
 }
